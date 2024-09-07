6d5f9b881ee0fff6416fc37b498d15ed97743c14
Jnanasrinivas-art <jnanasrinivas1995@gmail.com>
2024-09-07 15:45:44 -0700

    second commit

--- src/test/java/Tests/Test_01.java
+++ src/test/java/Tests/Test_01.java
@@ -10,13 +10,13 @@ public class Test_01 extends BaseTest
 {
     String finalSucMsg ="THANKYOU FOR THE ORDER.";
     String confirmationMsg="";
-      @Test(priority = 1,description = "Login into the Application")
+      @Test(priority = 1,groups = {"smoke","regression"},description = "Login into the Application")
       public void Test1() throws InterruptedException, IOException
       {
           land_page.loginApplication();
       }
 
-      @Test(priority = 2,description = "Select the Item and Add it into the cart and navigate to cartpage")
+      @Test(priority = 2,groups = {"smoke","regression"},description = "Select the Item and Add it into the cart and navigate to cartpage")
       public void Test2() throws InterruptedException, IOException
       {
           cart_page.selectProduct();
@@ -24,7 +24,7 @@ public class Test_01 extends BaseTest
           Thread.sleep(2400);
       }
 
-    @Test(priority = 3,description = "Navigate  to CartPage and Checkout")
+    @Test(priority = 3,groups = "smoke",description = "Navigate  to CartPage and Checkout")
     public void Test3() throws InterruptedException, IOException
     {
         checkout_page.checkProduct();
