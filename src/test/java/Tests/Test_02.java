e1c891874aff450fa4ec02ee62d0691e0b7a8907
Jnana Srinivas Garapati <jnanasrinivas1995@gmail.com>
2024-09-21 00:44:28 -0700

    Update Test_02.java

--- src/test/java/Tests/Test_02.java
+++ src/test/java/Tests/Test_02.java
@@ -1,29 +1,30 @@
 package Tests;
 
 import Basecomponent.BaseTest;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 
 public class Test_02 extends BaseTest
 {
-    String testData_Sheet = "Test-sheet";
     String finalSucMsg ="THANKYOU FOR THE ORDER.";
     String confirmationMsg="";
 
     @Test(priority = 1,description = "Login into the Application")
-    public void Test2() throws InterruptedException, IOException {
-        abstrct_comp.loginApplication(testData_Sheet, 3);
-        cart_page.selectProduct(testData_Sheet, 3);
+    public void Test2() throws InterruptedException, IOException
+    {
+        abstrct_comp.loginApplication();
+        cart_page.selectProduct();
         cart_page.clickCartPage();
-        checkout_page.checkProduct(testData_Sheet, 3);
-        payment_page.selectCountry(testData_Sheet, 3);
+        checkout_page.checkProduct();
+        payment_page.selectCountry();
         confirmationMsg = payment_page.validateConfirmationMsg();
-        Assert.assertEquals(confirmationMsg, finalSucMsg);
+        Assert.assertEquals(confirmationMsg,finalSucMsg);
+    }
+
+    void test3()
+    {
     }
 
 }
