9ab9dbdeab63352fdbce64d7cdd0782dce4a5ef2
Jnana Srinivas Garapati <jnanasrinivas1995@gmail.com>
2024-09-21 00:52:10 -0700

    Update Test_01.java

--- src/test/java/Tests/Test_01.java
+++ src/test/java/Tests/Test_01.java
@@ -39,4 +39,9 @@ public class Test_01 extends BaseTest
         confirmationMsg = payment_page.validateConfirmationMsg();
         Assert.assertEquals(confirmationMsg,finalSucMsg);
     }
+
+    void test5()
+    {
+        
+    }
 }
