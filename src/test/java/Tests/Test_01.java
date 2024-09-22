a3b3b805dac5a3607d638119d6f4add4bd761e7a
Jnanasrinivas-art <jnanasrinivas1995@gmail.com>
2024-09-21 18:20:34 -0700

    asd

--- src/test/java/Tests/Test_01.java
+++ src/test/java/Tests/Test_01.java
@@ -39,9 +39,5 @@ public class Test_01 extends BaseTest
         confirmationMsg = payment_page.validateConfirmationMsg();
         Assert.assertEquals(confirmationMsg,finalSucMsg);
     }
-
-    void test5()
-    {
-
-    }
+    
 }
